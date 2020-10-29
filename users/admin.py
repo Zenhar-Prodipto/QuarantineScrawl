a94f0e3f42fe503009681a622443959443d578ef
Zenhar-Prodipto <zenhar.mz@gmail.com>
2020-10-29 17:55:55 +0600

    added comments and like models

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Comment, Like
+
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Like)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from blog.models import Post
 from PIL import Image
 
 # Create your models here.
@@ -8,6 +9,7 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="poseidon.jpg", upload_to="profile_pics")
+    follows = models.ManyToManyField(User, related_name="+")
 
     def __str__(self):
         return f"{self.user.username} Profile"
@@ -19,6 +21,23 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_comment = models.TextField(null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Comment"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username}'s Like"
 
 
 # def save(self, *args, **kwargs):
