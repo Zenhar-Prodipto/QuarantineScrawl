da3ab9e42556ed0e5c1c6d2271925de69a6aa16c
Zenhar-Prodipto <zenhar.mz@gmail.com>
2021-02-18 20:14:28 +0600

    Redesigned the followers-list page

--- users/views.py
+++ users/views.py
@@ -116,7 +116,7 @@ def profileUpdateView(request):
 
 class followingListView(LoginRequiredMixin, ListView):
     model = Profile
-    template_name = "users/profile_following_list2.html"
+    template_name = "users/profile_following_list.html"
 
     # def get_followers(self):
     #     loggedInProfile = Profile.objects.get(user=self.request.user)
@@ -144,7 +144,7 @@ class followingListView(LoginRequiredMixin, ListView):
 
 class followerListView(LoginRequiredMixin, ListView):
     model = Profile
-    template_name = "users/profile_follower_list.html"
+    template_name = "users/profile_follower_list2.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
