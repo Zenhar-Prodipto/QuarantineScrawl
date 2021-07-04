0288938d5dc527a9f3323948fe3b2b42aa858852
Zenhar-Prodipto <zenhar.mz@gmail.com>
2021-07-04 17:51:47 +0600

    deployment

--- socialBlog/settings.py
+++ socialBlog/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["qurantinescrawl.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["quarantinescrawl.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
